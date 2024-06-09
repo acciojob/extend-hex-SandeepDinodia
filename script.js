5dd082b94ac98cf648bfa3eb36b44f7233f8bb8b
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-06-09 11:16:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	if(shortHex.startsWith('#')){
+		shortHex=shortHex.slice(1);
+	}
+	if(shortHex.lenght !== 3){
+		 throw new Error("Input must be a 3-character hex code.");
+	}
+	let fullHex="#";
+	for(let i=0; i<shortHex.length; i++){
+		fullHex+=shortHex[i]+shortHex[i];
+	}
+	return fullHex;
 };
 
 // Do not change the code below.
